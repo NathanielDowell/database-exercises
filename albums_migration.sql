23cc5d1d8d76801459495d9a905ba118a2a76deb
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-06-03 13:52:02 -0500

    edited table after walkthrough

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO-INCREMENT,
-    artist VARCHAR(100),
-    name VARCHAR(255),
-    release_date DATE ,
+    artist VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    release_date int NOT NULL,
     sales DECIMAL(11,2),
-    genre VARCHAR(50),
+    genre VARCHAR(255) NOT NULL,
     PRIMARY KEY (id);
 )
