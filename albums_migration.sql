3f52736c575649f46162d2efb0b9a44a43a91781
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-06-03 14:29:45 -0500

    syntax fixes

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO-INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     release_date int NOT NULL,
     sales DECIMAL(11,2),
     genre VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id);
+    PRIMARY KEY (id)
 )
